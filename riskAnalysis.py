05d4e0400afd0124878c0a62f35301479a5a863c
Dreizan <dreizan@seemoinfo.com>
2017-03-28 21:59:36 -0500

    Updated checkLicensing

--- riskAnalysis.py
+++ riskAnalysis.py
@@ -6,33 +6,50 @@ from pprint import pprint
 # Function which looks for licensing
 def checkLicensing(targetZip, zipDir):
 	# Gather zip name
-	zipName = targetZip[targetZip.rfind('/'):-4]
+	zipName = targetZip[targetZip.rfind('/'):]
+	hasLicenses = 0
+	hasCopyrights = 0
+
+	print(zipDir + "\n")
 
 	# Run Scancode to gather licensing information
-	os.system("./scancode-toolkit-1.6.0/scancode -f json -l -c " + zipDir + " > ./test/results/" + zipName + ".json")
+	os.system("./scancode-toolkit-1.6.0/scancode -f json -l -c " + zipDir + " > ./test/results" + zipName + ".json")
 
 	# Read through JSON data
-	with open("./test/results/" + zipName + '.json') as data_file:    
-    	data = json.load(data_file)
+	with open("./test/results" + zipName + '.json') as data_file:    
+    		data = json.load(data_file)
 
 	# pprint(data)
 
-    if(len(data['results']['licenses']) > 0):
-    	print("There are licenses included!\nLicenses:")
-    	for item in data['results']['licenses']:
-    		print(" " + item['short_name'])
+	for item in data['results']:
+		if(len(item['licenses']) > 0):
+			if(hasLicenses == 0):
+				print("There are licenses included!\nLicenses:")
+			hasLicenses = 1
+			for licns in item['licenses']:
+   				print(" " + licns['short_name'])
 
-    else:
-    	print("No licensing included.\n")
+	if(hasLicenses == 0):
+    		print("No licensing included.\n")
 
-    # os.system("rm -rf " + zipDir + "/..")
+   	for item in data['results']:
+		if(len(item['copyrights']) > 0):
+			if(hasCopyrights == 0):
+				print("There are copyrights included!\nCopyrights:")
+			hasCopyrights = 1
+			for cpy in item['copyrights']:
+   				print(" \"" + cpy['statements'] + "\"")
+
+	if(hasCopyrights == 0):
+   		print("No copyrights included.\n")
+    
 
 # Function that checks if the passed repository URL exists
-def checkURL():
+def checkURL(targetURL):
    inZip = ''
    outAnalysis = ''
 
-   request = requests.head(sys.argv[1])
+   request = requests.head(targetURL)
    if (request.status_code == 200):
       print("URL exists!\n")
       
@@ -50,12 +67,22 @@ def unzipFile(targetZip):
 	theZip.extractall(zipDirectory)
 	theZip.close()
 
-	return(targetZip[:-1] + "/" + targetZip[targetZip.rfind('/'):-4])
+	return(targetZip[:-1])
+
+def getZip(targetURL):
+	#Grab the zip of the user passed in URL
+	zipName = targetURL[targetURL.rfind('/')+1:]
+	print(zipName + ".zip")
+	os.system("curl -L " + targetURL + "/zipball > " + zipName + ".zip")
+	return(zipName + ".zip")
+	#os.system("curl -L https://api.github.com/repos/Dreizan/Repos-Health-Study/zipball > Repos-Health-Study.zip")
+	#os.system("curl -L https://github.com/Dreizan/Repos-Health-Study/zipball > Repos-Health-Study.zip")
 
 # Main
 def main():
-	# checkURL()
-	zipDir = unzipFile(sys.argv[1])
+	checkURL(sys.argv[1])
+	zipF = getZip(sys.argv[1])
+	zipDir = unzipFile(zipF)
 	checkLicensing(sys.argv[1], zipDir)
 
 if __name__ == "__main__":
